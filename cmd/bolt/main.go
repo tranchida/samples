0ea22cb4dae5ecb018d1148a1df0367d438157ea
Giampaolo Tranchida <giampaolo.tranchida@vd.ch>
2025-01-17 18:25:08 +0100

    add bolt

---
+++ cmd/bolt/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/fatih/color"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+type message struct {
+	MessageId             string            `json:"messageId"`
+	BusinessId            string            `json:"businessId"`
+	BusinessCorrelationId string            `json:"businessCorrelationId"`
+	Headers               map[string]string `json:"headers"`
+	ServiceDestination    string            `json:"serviceDestination"`
+	Body                  string            `json:"body"`
+}
+
+
+func main() {
+
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) error {
+
+		b, err := tx.CreateBucketIfNotExists([]byte("MyBucket2"))
+		if err != nil {
+			panic(err)
+		}
+
+		m := message{
+			MessageId:             "1",
+			BusinessId:            "1",
+			BusinessCorrelationId: "1",
+			Headers: map[string]string{
+				"key1": "value1",
+				"key2": "value2",
+			},
+			ServiceDestination: "service1",
+			Body:               "<test/>",
+		}
+
+		j, err := json.Marshal(m)
+		if err != nil {
+			panic(err)
+		}
+		b.Put([]byte("2"), j)
+
+		return nil
+	})
+
+	db.View(func(tx *bolt.Tx) error {
+
+		b := tx.Bucket([]byte("MyBucket"))
+		if b == nil {
+			return nil
+		}
+
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			
+			color.Red("key -> %s\n", string(k))
+			color.Blue("value -> %s\n", string(v))
+		}
+
+		return nil
+	
+	})
+
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	go.etcd.io/bbolt v1.3.11
 	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 )
